a1fa2eec76f0ddf8ecda9307bfb35b9bb6b93861
kborthakur <36449255+kborthakur@users.noreply.github.com>
2022-03-29 13:41:46 +0530

    Create Main.java

---
+++ Assignments/Datatypes & Variables - Post Class/Magic Trick/Main.java
@@ -0,0 +1,21 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+                      int x = 3;
+                      Scanner in = new Scanner(System.in);
+                      int a = in.nextInt();
+                      int b = in.nextInt();
+
+                      int z = (((2 * (x + a)) + b)/2) - x;
+                      System.out.print(z);
+
+
+
+
+	}
+}
