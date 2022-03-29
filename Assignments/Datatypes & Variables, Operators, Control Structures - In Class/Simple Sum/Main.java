a04eaed2bdd778c751dcd1297f6f02ea0441be3f
kborthakur <36449255+kborthakur@users.noreply.github.com>
2022-03-29 13:45:33 +0530

    Create Main.java

---
+++ Assignments/Datatypes & Variables, Operators, Control Structures - In Class/Simple Sum/Main.java
@@ -0,0 +1,16 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+					  Scanner in = new Scanner(System.in);
+					  int a = in.nextInt();
+					  int b = in.nextInt();
+					  int c = in.nextInt();
+					  int z = a + b + c;
+					  System.out.print(z);
+	}
+}
