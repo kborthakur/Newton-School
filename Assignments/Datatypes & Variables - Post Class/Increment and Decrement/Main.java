18f282066aa977c46e1db64e144f536e8d12f280
kborthakur <36449255+kborthakur@users.noreply.github.com>
2022-03-29 13:39:59 +0530

    Create Main.java

---
+++ Assignments/Datatypes & Variables - Post Class/Increment and Decrement/Main.java
@@ -0,0 +1,18 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+					  Scanner in = new Scanner(System.in);
+					  int x = in.nextInt();
+					  int y = in.nextInt();
+
+					  x -= 1;
+					  y += 1;
+
+					  System.out.print(x + " " + y);
+	}
+}
